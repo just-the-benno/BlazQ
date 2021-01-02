698dc1f6fdf0ab8a28677518634f88968ccc16ad
Benjamin Kappel <b.kappel90@outlook.de>
2021-01-02 18:12:47 +0800

    first episode first try

---
+++ episode1/BlazQ.Episode1Finished/BlazQ.Episode1/SimplePathPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazQ.Episode1
+{
+    public class SimplePathPoint
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        [Required]
+        [Range(0,100)]
+        public Double X { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public Double Y { get; set; }
+
+    }
+}
