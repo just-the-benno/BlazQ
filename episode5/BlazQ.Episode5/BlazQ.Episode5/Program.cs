08a6005d15ccf52cf590ae31c35ed6a752fdec40
Benjamin Kappel <b.kappel90@outlook.de>
2021-02-19 02:26:23 +0800

    ep05 preparation

--- episode5/BlazQ.Episode5/BlazQ.Episode5/Program.cs
+++ episode5/BlazQ.Episode5/BlazQ.Episode5/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using BlazQ.Episode4;
+using BlazQ.Episode5;
 
 namespace BlazQ
 {
